9ff36ef0b70d2d5d664fc7ff5e074714ad1e976a
shreyaporwal2003 <shreyaporwal167@gmail.com>
2026-02-03 15:30:50 +0530

    changes made in socket.io

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
-// Current laptop IP (for localhost + phone testing)
-const socket = io("http://10.108.143.140:5000", {
-  transports: ["websocket"],
-  reconnectionAttempts: 5
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["websocket"]
 });
 
 export default socket;
