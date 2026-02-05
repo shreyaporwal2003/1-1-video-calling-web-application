b98cd37b3942f139b80b0c5ab74c95727ca280a2
shreyaporwal2003 <shreyaporwal167@gmail.com>
2026-02-05 16:12:00 +0530

    change in url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.VITE_BACKEND_URL, {
-  transports: ["websocket"]
+// Use backend URL from Vite environment variable
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["websocket"],
+  withCredentials: true
 });
 
 export default socket;
